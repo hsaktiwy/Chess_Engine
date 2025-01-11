4297e7fee89daf8f9ab7d7b21d7db202a050116e
hsaktiwy <hsaktiwy@gmail.com>
2025-01-11 10:57:13 +0100

    initial foot to the engine

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ChessGUI from './chessGUI/ChessGui';
+
+function App() {
+  return (
+    <div className="App">
+      <ChessGUI/>
+    </div>
+  );
+}
+
+export default App;
